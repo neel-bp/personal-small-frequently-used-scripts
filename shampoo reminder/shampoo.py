8cd987ab214100fa28c4704e55dd26567046c97c
neelu0 <neelu0@protonmail.com>
2019-07-04 15:31:14 +0500

    added notifier to shampoo reminder and gitignore

--- shampoo reminder/shampoo.py
+++ shampoo reminder/shampoo.py
@@ -1,5 +1,6 @@
 from datetime import date
 import yaml
+from plyer import notification
 
 conf=yaml.safe_load(open('todate.yaml'))
 todaydate=date.today()
@@ -8,7 +9,12 @@ shampoodate=conf['lastshampooed']
 diff=date.today()-shampoodate
 
 if diff.days == conf['threshold']:
-	print("it's shampoo day motherfucker")
+	notification.notify(
+		title="Its shampoo day my dood",
+		message="Yup you gotta shampoo today and take a break from fapping",
+		app_icon=None,
+		timeout=10
+	)
 	updateconf=yaml.safe_load(open('todate.yaml'))
 	updateconf['lastshampooed']=date.today()
 	yaml.dump(updateconf, open('todate.yaml','w'))
