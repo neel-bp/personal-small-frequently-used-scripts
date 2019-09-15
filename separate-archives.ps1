e296b56768bd2fc83285c136e085ca828dc85459
neelu0 <45328066+neelu0@users.noreply.github.com>
2019-09-15 18:41:21 +0500

    rewrote it and wrapped it around a function and made it a module

--- separate-archives.ps1
+++
@@ -1,6 +0,0 @@
-$dirs = (ls -Name)
-foreach ($dir in $dirs){
-    if ($dir -ne "separate-archives.ps1") {
-        7z.exe a "$($dir).zip" "$($dir)"    
-    }
-}
---
+++ separate-archives.psm1
@@ -0,0 +1,8 @@
+function separateArchive{
+    $dirs = (ls -Name)
+    foreach ($dir in $dirs){
+        .\7z a "$($dir).zip" "$($dir)"
+    }
+}
+
+#either put the function in profile or just put it somewhere and import the function with import-module
