1bcd37f0887e4df246851d185d2019563f1aa146
neelu0 <neelu0@protonmail.com>
2019-10-11 15:02:03 +0500

    added option to choose editor in for bulkrenaming with bulkrename module

--- bulkrename.psm1
+++ bulkrename.psm1
@@ -1,11 +1,15 @@
-﻿function bulkrename {
+﻿function bulkrename($editor="vim") {
 $sourcetemp = New-TemporaryFile
 $targettemp = New-TemporaryFile
 
 ls -Name | Out-File $sourcetemp.FullName -Encoding utf8
 ls -Name | Out-File $targettemp.FullName -Encoding utf8
 
-Start-Process -FilePath "vim" -ArgumentList "-u","NONE","`"$($targettemp.FullName)`"" -Wait -NoNewWindow
+if ($editor -eq "vim"){
+    Start-Process -FilePath "$($editor)" -ArgumentList "-u","NONE","`"$($targettemp.FullName)`"" -Wait -NoNewWindow
+} else {
+    Start-Process -FilePath "$($editor)" -ArgumentList "`"$($targettemp.FullName)`"" -Wait
+}
 
 $sourceArr = Get-Content $sourcetemp.FullName
 $targetArr = Get-Content $targettemp.FullName
